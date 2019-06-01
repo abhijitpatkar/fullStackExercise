9851f366013ef354d583d6998ae8ae8bd8fd8710
abhijitpatkar <abhijit.patkar@gmail.com>
2019-06-02 02:28:55 +0530

    Updated the code and its working code based on the requirement

--- iridize/static/iridize/player.js
+++ iridize/static/iridize/player.js
@@ -1,33 +1,17 @@
 $(document).ready(function(){
-    /*$.getJSON('/static/iridize/guide.json',function(data){
-        console.log(data);
-
-
-     });*/
-     $.ajax({
-     url:'/static/iridize/guide.json',
-     dataType:'json',
-     type:'get',
-     cache:false,
-     success: function(data){
-
-     var firstTip = data['steps'][0]['contents'];
-     var secondTip =  data['steps'][1]['contents'];
-     var thirdTip =  data['steps'][2]['contents'];
-
-     //firtElement= document.getElementById("id_1");
-     //firtElement.innerHTML = firstTip
-
-        /*$(data.steps).each(function(key, value){
-
-        });*/
-      }
-
-      });
-
+     var responseDict ={}
+     //alert("Hi");
+     $(responseJson.steps).each(function(key, value){
+             responseDict[value.id] = value;
+        });
+$('#id_next').click( function(){
+//alert("Next clicked");
+//alert(this.value)
+resObj = responseDict[this.value]
+$(resObj.selector).text(resObj.content)
+this.value = resObj.next
 
 });
 
 
-
-
+});
--- iridize/views.py
+++ iridize/views.py
@@ -2,11 +2,28 @@ from django.shortcuts import render,render_to_response
 from django.http import HttpResponse
 import json
 # Create your views here.
-"""
-def home(request):
-
-    return HttpResponse('<h1> Home Page </h1>')
-"""
 
 def home(request):
-    return render(request, 'iridize/index.html')
+    jsonData= """{
+        "steps": [
+            {
+                "id": "1",
+                "content": "tip on first div",
+                "selector": "#id_1",
+                "next": "2"
+            },
+            {
+                "id": "2",
+                "content": "tip on second div",
+                "selector": ".myClass2",
+                "next": "3"
+            },
+            {
+                "id": "3",
+                "content": "tip on third div.",
+                "selector": "div:eq(2)",
+                "next": None # null is replaced with None as null is not valied in python
+            }
+        ]
+    }"""
+    return render(request, 'iridize/index.html',{'jsonData':json.dumps(jsonData)})
